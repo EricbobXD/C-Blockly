8e4ad28d18e326d6bb1bac14170f5d766a92681c
Coderex._.bob <140780835+EricbobXD@users.noreply.github.com>
2024-09-26 09:16:09 +0800

    Update main.js

--- main.js
+++ main.js
@@ -12,12 +12,10 @@ var workspace = Blockly.inject('blocklyDiv', {
                 <category name="主程式框架" colour="0">
                     <block type="include_bits/stdc++.h">
                         <next>
-                            <block type="using_namespace_std"></block>
-                        </next>
-                        <next>
                             <block type="main_block"></block>
                         </next>
                     </block>
+                    <block type="using_namespace_std"></block>
                 </category>
                 
                 <category name="自定義函式" colour="290">
