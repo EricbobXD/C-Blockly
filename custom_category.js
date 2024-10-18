8d37de65b6a4cea62eb7f761afac8c7a0d2dbabf
Coderex._.bob <140780835+EricbobXD@users.noreply.github.com>
2024-10-18 16:01:15 +0800

    Update custom_category.js

--- custom_category.js
+++ custom_category.js
@@ -6,14 +6,14 @@ class CustomCategory extends Blockly.ToolboxCategory {
       var labelDom = this.rowDiv_.getElementsByClassName('blocklyTreeLabel')[0];
       if (isSelected) {
         // Change the background color of the div to white.
-        this.rowDiv_.style.backgroundColor = 'gray';
+        this.rowDiv_.style.backgroundColor = 'white';
         // Set the colour of the text to the colour of the category.
         labelDom.style.color = this.colour_;
       } else {
         // Set the background back to the original colour.
         this.rowDiv_.style.backgroundColor = this.colour_;
         // Set the text back to white.
-        labelDom.style.color = 'white';
+        labelDom.style.color = 'black';
       }
       // This is used for accessibility purposes.
       Blockly.utils.aria.setState(/** @type {!Element} */ (this.htmlDiv_),
